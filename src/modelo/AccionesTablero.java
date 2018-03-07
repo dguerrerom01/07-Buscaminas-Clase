74b9872f7f903ee1a8465fe14175c08cdafbf86d
ionicHarnina <ionicHarnina@gmail.com>
2018-03-07 09:34:35 +0100

    el del metodo recursivo y las banderitas

--- src/modelo/AccionesTablero.java
+++ src/modelo/AccionesTablero.java
@@ -5,4 +5,5 @@ public interface AccionesTablero {
 	 * por cada sitio, calcula el número de minas que tiene alrededor
 	 */
 	void calcularMinasAlrededor();
+	void desvelarContigua(Coordenada lugar);
 }
